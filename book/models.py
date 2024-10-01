c8b41f8655355f70a7127b057c01ff25f16d486b
rompu <rom.puh@gmail.com>
2024-10-01 12:05:39 +0300

    Revert ""
    This reverts commit 6c04dbed7c04dc688bad86a9f42362eed8c350b3.

--- book/models.py
+++ book/models.py
@@ -1,19 +1,3 @@
 from django.db import models
 
-
-class Book(models.Model):
-    class CoverChoices(models.TextChoices):
-        HARD = "Hard"
-        SOFT = "Soft"
-
-    title = models.CharField(max_length=256)
-    author = models.CharField(max_length=256)
-    cover = models.CharField(max_length=50, choices=CoverChoices.choices)
-    inventory = models.PositiveIntegerField()
-    daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
-
-    class Meta:
-        ordering = ["title"]
-
-    def __str__(self):
-        return f"{self.title} - {self.author}, cover: {self.cover}"
+# Create your models here.
