9dadea8b4a4e2f586b46d7d0f1aba5a6474a038f
rompu <rom.puh@gmail.com>
2024-10-01 15:24:19 +0300

    created endpoint books

--- book/views.py
+++ book/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import (
+    extend_schema,
+    OpenApiParameter,
+)
 
-# Create your views here.
+from book.models import Book
+from book.serializers import BookSerializer, BookListSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BookListSerializer
+        return BookSerializer
+
+    @staticmethod
+    def _params_to_ints(query_sting):
+        """Convert a string of format '1,2,3' to a list of integers [1,2,3]"""
+        return [int(str_id) for str_id in query_sting.split(",")]
+
+    def get_queryset(self):
+        queryset = self.queryset
+        title = self.request.query_params.get("title")
+        author = self.request.query_params.get("author")
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        if author:
+            queryset = queryset.filter(author__icontains=author)
+        return queryset.distinct()
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="title",
+                type=OpenApiTypes.STR,
+                description="Filtering by title (ex. ?title=Book)",
+            ),
+            OpenApiParameter(
+                name="author",
+                type=OpenApiTypes.STR,
+                description="Filtering by author (ex. ?author=Name)",
+            )
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
