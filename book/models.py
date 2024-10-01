5519ec55418d5edfc8001eb6f5d170e80ef472b4
rompu <rom.puh@gmail.com>
2024-10-01 15:19:38 +0300

    added default value to inventory field

--- book/models.py
+++ book/models.py
@@ -9,8 +9,8 @@ class Book(models.Model):
     title = models.CharField(max_length=256)
     author = models.CharField(max_length=256)
     cover = models.CharField(max_length=50, choices=CoverChoices.choices)
-    inventory = models.PositiveIntegerField()
-    daily_fee = models.DecimalField(decimal_places=2)
+    inventory = models.PositiveIntegerField(default=0)
+    daily_fee = models.DecimalField(decimal_places=2, max_digits=7)
 
     def __str__(self):
         return f"{self.title} - author: {self.author}"
