797febe9055ace7985c277d8f40f498ef153242e
rompu <rom.puh@gmail.com>
2024-10-22 09:48:05 +0300

    created views.py to payment

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from payment.models import Payment
+from payment.serializers import (
+    PaymentSerializer,
+    PaymentListSerializer,
+    PaymentDetailSerializer,
+)
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PaymentListSerializer
+        if self.action == "retrieve":
+            return PaymentDetailSerializer
+        return PaymentSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(user=self.request.user)
+        if self.action in ["list", "retrieve"]:
+            return queryset.select_related("borrowing")
+        return queryset
