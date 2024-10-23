c2cbbe9150736a24dc54bcb9bbc3923487067b01
rompu <rom.puh@gmail.com>
2024-10-23 17:11:16 +0300

    added checking the number of pending payments & if it exists, forbid borrowing

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from book.serializers import BookSerializer
 from payment.serializers import PaymentListSerializer
 from borrowing.models import Borrowing
+from payment.models import Payment
 
 
 class BorrowingListSerializer(serializers.ModelSerializer):
@@ -55,8 +56,19 @@ class BorrowingCreateSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
         book = data.get("book")
+        user = self.context["request"].user
+        pending_payments = Payment.objects.filter(
+            borrowing__user=user, status=Payment.StatusChoices.PENDING
+        )
+
+        if pending_payments.exists():
+            raise serializers.ValidationError(
+                "This borrowing is forbidden. "
+                "You have to complete your pending payment"
+            )
         if book.inventory == 0:
             raise serializers.ValidationError("There isn't available book")
+
         return data
 
     def create(self, validated_data):
